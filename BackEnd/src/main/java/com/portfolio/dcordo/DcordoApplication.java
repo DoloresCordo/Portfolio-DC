e72885bb43c2842d39a56b97251315acc621b697
DoloresCordo <dolorescordo@gmail.com>
2022-06-07 18:38:41 -0300

    Creacion de carpeta Backend
    Creacion de crapeta Backend

---
+++ BackEnd/src/main/java/com/portfolio/dcordo/DcordoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.dcordo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcordoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcordoApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portfolio/dcordo/DcordoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.dcordo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcordoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
